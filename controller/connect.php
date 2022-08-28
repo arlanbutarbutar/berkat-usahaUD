3865875aa1f0f33df8731b4ff95bfe507df9904b
arlanbutarbutar <arlan270899@gmail.com>
2022-08-28 20:32:15 +0800

    update

--- controller/connect.php
+++ controller/connect.php
@@ -1,5 +1,5 @@
 <?php 
-    $conn=mysqli_connect("localhost","root","","berkat_usahaUD");
+    $conn=mysqli_connect("localhost","root","","ud_berkat");
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
